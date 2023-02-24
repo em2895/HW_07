93046a3bb8550297f42392e8706c2fe7c84ef727
Emil Akhmedov <emil282@ukr.net>
2023-02-24 17:55:48 +0200

    done HW_07

---
+++ script.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("Введіть перше число: "));
+let b = Number(prompt("Введіть друге число: "));
+let c = Number(prompt("Введіть третє число: "));
+
+let average = (a + b + c) / 3;
+
+alert("Середнє арифметичне чисел " + a + ", " + b + ", " + c + ", " + " дорівнює " + average);
